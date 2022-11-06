be581c927fd0452bfe94116cb09432a9ccb2a5d8
Ketib Oldiais <numerstack@gmail.com>
2022-11-06 13:32:25 -0600

    Begin migration from TypeScript. Include common.h and main.c.

---
+++ slang/common.h
@@ -0,0 +1,15 @@
+//
+//  common.h
+//  slang
+//
+//  Created by Ketib Oldiais on 11/6/22.
+//
+
+#ifndef common_h
+#define common_h
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif /* common_h */
