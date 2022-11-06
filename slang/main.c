88913d830f9b5b82f6e7a9921f2f89dfba9ed77e
Ketib Oldiais <numerstack@gmail.com>
2022-11-06 13:28:08 -0600

    Initial Commit

---
+++ slang/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  slang
+//
+//  Created by Ketib Oldiais on 11/6/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
